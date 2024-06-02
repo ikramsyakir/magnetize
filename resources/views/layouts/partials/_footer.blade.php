f52b970091bb083af03e2689007cd8dc32321324
Ikram Syakir <ikramsyakir@gmail.com>
2024-06-03 05:46:12 +0800

    ⬆️ Update to latest version laravel

--- resources/views/layouts/partials/_footer.blade.php
+++ resources/views/layouts/partials/_footer.blade.php
@@ -1,10 +1,13 @@
+@php
+    use Carbon\Carbon;
+@endphp
 <footer class="footer footer-transparent d-print-none">
     <div class="container">
         <div class="row text-center align-items-center">
             <div class="col-12 col-lg-auto mt-3 mt-lg-0">
                 <ul class="list-inline list-inline-dots mb-0">
                     <li class="list-inline-item">
-                        Copyright &copy; 2021 {{ config('app.name') }}. All rights reserved.
+                        Copyright &copy; {{ Carbon::now()->year }} {{ config('app.name') }}. All rights reserved.
                     </li>
                 </ul>
             </div>

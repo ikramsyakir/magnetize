1d859f4dcbb26dbd420d005987d3e2c0ec9d57ee
Ikram Syakir <ikramsyakir@gmail.com>
2024-06-08 03:40:33 +0800

    wip login

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -3,7 +3,7 @@
 @section('title', 'Login')
 
 @section('main-content')
-    <div id="app" class="container-tight py-4">
+    <div id="app" class="container container-tight py-4">
         <div class="text-center mb-4">
             <a href="{{ url('/') }}"><img src="{{ asset('images/magnetize-logo.png') }}" height="60" alt=""></a>
         </div>
@@ -14,11 +14,12 @@
                 <h2 class="card-title text-center mb-4">Login to your account</h2>
                 <div class="mb-3">
                     <label class="form-label">Username / Email</label>
-
-                    <input id="login" type="text" class="form-control {{ $errors->has('username') || $errors->has('email') ? ' is-invalid' : '' }}" name="login" value="{{ old('username') ?: old('email') }}" required autofocus>
+                    <input id="login" type="text"
+                           class="form-control {{ $errors->has('username') || $errors->has('email') ? ' is-invalid' : '' }}"
+                           name="login" value="{{ old('username') ?: old('email') }}" required autofocus>
 
                     @if ($errors->has('username') || $errors->has('email'))
-                    <span class="invalid-feedback" role="alert">
+                        <span class="invalid-feedback" role="alert">
                         <strong>{{ $errors->first('username') ?: $errors->first('email') }}</strong>
                     </span>
                     @endif
@@ -32,10 +33,12 @@
                     </label>
 
                     <div id="show_hide_password" class="input-group input-group-flat">
-                        <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="current-password">
+                        <input id="password" :type="passwordType"
+                               class="form-control @error('password') is-invalid @enderror" name="password" required
+                               autocomplete="current-password">
                         <span class="input-group-text">
-                            <a href="#" class="link-secondary" title="Show password">
-                                <i class="fa fa-eye-slash"></i>
+                            <a class="link-secondary" title="Show password" @click="togglePassword">
+                                <i :class="passwordClass"></i>
                             </a>
                         </span>
                     </div>
@@ -48,27 +51,13 @@
                 </div>
                 <div class="mb-2">
                     <label class="form-check">
-                        <input class="form-check-input" type="checkbox" name="remember" id="remember" {{ old('remember') ? 'checked' : '' }}>
+                        <input class="form-check-input" type="checkbox" name="remember"
+                               id="remember" {{ old('remember') ? 'checked' : '' }}>
                         <span class="form-check-label">Remember me on this device</span>
                     </label>
                 </div>
                 <div class="form-footer">
                     <button type="submit" class="btn btn-primary w-100">Sign in</button>
-                </div>
-            </div>
-            <div class="hr-text">or</div>
-            <div class="card-body">
-                <div class="row">
-                    <div class="col"><a href="#" class="btn btn-white w-100">
-                            <!-- Download SVG icon from http://tabler-icons.io/i/brand-github -->
-                            <svg xmlns="http://www.w3.org/2000/svg" class="icon text-github" width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M9 19c-4.3 1.4 -4.3 -2.5 -6 -3m12 5v-3.5c0 -1 .1 -1.4 -.5 -2c2.8 -.3 5.5 -1.4 5.5 -6a4.6 4.6 0 0 0 -1.3 -3.2a4.2 4.2 0 0 0 -.1 -3.2s-1.1 -.3 -3.5 1.3a12.3 12.3 0 0 0 -6.2 0c-2.4 -1.6 -3.5 -1.3 -3.5 -1.3a4.2 4.2 0 0 0 -.1 3.2a4.6 4.6 0 0 0 -1.3 3.2c0 4.6 2.7 5.7 5.5 6c-.6 .6 -.6 1.2 -.5 2v3.5" /></svg>
-                            Login with Github
-                        </a></div>
-                    <div class="col"><a href="#" class="btn btn-white w-100">
-                            <!-- Download SVG icon from http://tabler-icons.io/i/brand-twitter -->
-                            <svg xmlns="http://www.w3.org/2000/svg" class="icon text-twitter" width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M22 4.01c-1 .49 -1.98 .689 -3 .99c-1.121 -1.265 -2.783 -1.335 -4.38 -.737s-2.643 2.06 -2.62 3.737v1c-3.245 .083 -6.135 -1.395 -8 -4c0 0 -4.182 7.433 4 11c-1.872 1.247 -3.739 2.088 -6 2c3.308 1.803 6.913 2.423 10.034 1.517c3.58 -1.04 6.522 -3.723 7.651 -7.742a13.84 13.84 0 0 0 .497 -3.753c-.002 -.249 1.51 -2.772 1.818 -4.013z" /></svg>
-                            Login with Twitter
-                        </a></div>
                 </div>
             </div>
         </form>
@@ -80,26 +69,4 @@
 
 @push('scripts')
     @vite('resources/js/views/auth/login.js')
-{{--    <script type="module">--}}
-{{--        $(function(){--}}
-{{--            $("#show_hide_password a").on('click', function(event) {--}}
-
-{{--                event.preventDefault();--}}
-
-{{--                let input = '#show_hide_password input';--}}
-{{--                let icon = '#show_hide_password i';--}}
-
-{{--                if ($(input).attr("type") === "text"){--}}
-{{--                    $(input).attr('type', 'password');--}}
-{{--                    $(icon).addClass( "fa-eye-slash" );--}}
-{{--                    $(icon).removeClass( "fa-eye" );--}}
-{{--                } else if($(input).attr("type") === "password"){--}}
-{{--                    $(input).attr('type', 'text');--}}
-{{--                    $(icon).removeClass( "fa-eye-slash" );--}}
-{{--                    $(icon).addClass( "fa-eye" );--}}
-{{--                }--}}
-
-{{--            });--}}
-{{--        });--}}
-{{--    </script>--}}
 @endpush

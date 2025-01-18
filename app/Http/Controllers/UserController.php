18e62639031c82812618c9be1d69d8bfae648eaf
ikramsyakir <17622728+ikramsyakir@users.noreply.github.com>
2025-01-18 17:22:37 +0000

    PHP Linting (Pint)

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -13,7 +13,6 @@ use Illuminate\Contracts\View\Factory;
 use Illuminate\Contracts\View\View;
 use Illuminate\Http\JsonResponse;
 use Illuminate\Http\RedirectResponse;
-use Illuminate\Http\Request;
 use Illuminate\Routing\Redirector;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Storage;
